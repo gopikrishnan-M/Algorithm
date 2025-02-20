abab01176bf82adccc3eadac9e68b58ad44c7971
gopikrishnan-M <gopikris0021@gmail.com>
2025-02-20 12:34:04 +0530

    Java_Algos

---
+++ Searching_Sorting/BinarySearch.java
@@ -0,0 +1,23 @@
+package Searching_Sorting;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[]arr={2,4,5,3,5,3,9,378,45,34,23,12};
+        int low=0;
+        int high=arr.length-1;
+        int key=13;
+        int indx=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==key){
+                indx=mid;
+                break;
+            }
+            else if (arr[mid]>key)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        System.out.println(key+" element is found at index "+indx);
+    }
+}
